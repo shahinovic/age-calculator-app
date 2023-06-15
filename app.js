21fee3c830acc46e240b650bba03937f034199a0
shahinovic <shahinovic010101@gmail.com>
2023-06-15 07:03:50 +0300

    finish validation

--- app.js
+++ app.js
@@ -17,9 +17,36 @@ const calculate = () => {
   const yearInputValue = parseInt(yearInput.value);
 
   // Validation
-  if (isNaN(dayInputValue) || isNaN(monthInputValue) || isNaN(yearInputValue)) {
-    alert("Please enter valid numeric values for the date.");
+  if (isNaN(dayInputValue)) {
+    dayInput.classList.add("error");
+    document.querySelector('label[for="day"]').classList.add("error");
+    document.querySelector(
+      'label[for="day"]'
+    ).dataset.error = `Enter a valid day`;
     return;
+  }
+  if (isNaN(monthInputValue)) {
+    dayInput.classList.remove("error");
+    document.querySelector('label[for="day"]').classList.remove("error");
+    monthInput.classList.add("error");
+    document.querySelector('label[for="month"]').classList.add("error");
+    document.querySelector(
+      'label[for="month"]'
+    ).dataset.error = `Enter a valid Month`;
+    return;
+  }
+  if (isNaN(yearInputValue) || yearInputValue > currentYear) {
+    monthInput.classList.remove("error");
+    document.querySelector('label[for="month"]').classList.remove("error");
+    yearInput.classList.add("error");
+    document.querySelector('label[for="year"]').classList.add("error");
+    document.querySelector(
+      'label[for="year"]'
+    ).dataset.error = `Enter a valid Year`;
+    return;
+  } else {
+    yearInput.classList.remove("error");
+    document.querySelector('label[for="year"]').classList.remove("error");
   }
 
   if (
